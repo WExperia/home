9de4ab3f25daf1ec43bf5ca2f5f8dc9b3c5168f9
dieugenio <eduardo@dieugenio.net>
2014-12-11 21:51:30 +0100

    Actualización de la web de wexperia en clase

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<title>WExperia: Proyectos Web con Valor</title>
+		<link rel="stylesheet" href="styles/fonts.css" />
+		<style>
+		</style>
+	</head>
+	<body>
+		<h1 class="thin">MyriadSetPro-Thin</h1>
+		<h1 class="thin-italic">MyriadSetPro-Thin-Italic</h1>
+		<h1 class="bold">MyriadSetPro-Bold</h1>
+		<h1 class="bold-italic">MyriadSetPro-Bold-Italic</h1>
+	</body>
+</html>
