792e86bbe56227cfbcdd52e8c6c70a2f514052f8
Eduardo Di Eugenio <eduardo@dieugenio.net>
2014-12-12 08:03:09 +0100

    Ultimas actualizaciones en casa

---
+++ fonts.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        <meta name="HandheldFriendly" content="true">
+
+		<title>WExperia: Factoría de Proyectos Web</title>
+
+		<link rel="stylesheet" href="css/normalize.css" />
+		<link rel="stylesheet" href="css/wexperia.css" />
+	</head>
+	<body>
+
+		<h1 class="msp-th">MyriadSetPro-Thin</h1>
+		<h1 class="msp-th-it">MyriadSetPro-Thin-Italic</h1>
+		<h1 class="msp-bo">MyriadSetPro-Bold</h1>
+		<h1 class="msp-bo-it">MyriadSetPro-Bold-Italic</h1>
+		<h1 class="msp-ul">MyriadSetPro-Ultralight</h1>
+		<h1 class="msp-ul-it">MyriadSetPro-Ultralight-Italic</h1>
+		<h1 class="msp-sb">MyriadSetPro-Semibold</h1>
+		<h1 class="msp-sb-it">MyriadSetPro-Semibold-Italic</h1>
+
+	</body>
+</html>
--- index.php
+++ index.php
@@ -2,15 +2,85 @@
 <html lang="es">
 	<head>
 		<meta charset="utf-8">
-		<title>WExperia: Proyectos Web con Valor</title>
-		<link rel="stylesheet" href="styles/fonts.css" />
-		<style>
-		</style>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        <meta name="HandheldFriendly" content="true">
+
+		<title>WExperia: Factoría de Proyectos Web</title>
+
+		<link rel="stylesheet" href="css/normalize.css" />
+		<link rel="stylesheet" href="css/wexperia.css" />
+		<link rel="stylesheet" href="css/html5boilerplate.css" />
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+
 	</head>
 	<body>
-		<h1 class="thin">MyriadSetPro-Thin</h1>
-		<h1 class="thin-italic">MyriadSetPro-Thin-Italic</h1>
-		<h1 class="bold">MyriadSetPro-Bold</h1>
-		<h1 class="bold-italic">MyriadSetPro-Bold-Italic</h1>
+        <div class="header-container">
+            <header class="wrapper clearfix">
+                <h1 class="title">WExperia</h1>
+                <nav>
+                    <ul>
+                        <li><a href="#">Nosotros</a></li>
+                        <li><a href="#">Proyectos</a></li>
+                        <li><a href="#">Tecnología</a></li>
+                        <li><a href="#">Contacto</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+
+        <div class="main-container">
+            <div class="main wrapper clearfix">
+
+                <article>
+                    <header>
+                        <h2>Nosotros</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec.</p>
+                    </header>
+                    <section>
+                        <h2>Proyectos</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
+                    </section>
+                    <section>
+                        <h2>Tecnología</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
+                    </section>
+                    <footer>
+                        <h3>Contacto</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor.</p>
+                    </footer>
+                </article>
+
+                <aside>
+                    <h3>aside</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices.</p>
+                </aside>
+
+            </div> <!-- #main -->
+        </div> <!-- #main-container -->
+
+        <div class="footer-container">
+            <footer class="wrapper">
+                <h5>Este sitio se ha desarrollado como práctica del curso de Desarrollo y Publicación de Páginas Web.</h3>
+                <h6>Prohibida su reproducción de cualquier forma.</h3>
+                <p>&copy 2014 Eduardo Di Eugenio. Todos los derechos reservados</p>
+            </footer>
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="js/wexperia.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+        /*
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        */
+        </script>
 	</body>
 </html>
